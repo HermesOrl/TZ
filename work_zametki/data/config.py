755abbdb84aed55f8304443773b17ed5e5b46e33
HermesOrl <98828839+HermesOrl@users.noreply.github.com>
2023-11-16 20:20:22 +0300

    Add files via upload

--- work_zametki/data/config.py
+++ work_zametki/data/config.py
@@ -1,7 +1,7 @@
 import json
 
 
-def get_json_text():
+def get_json_text() -> dict:
 
     """
     Получение json с текстами для приложения
@@ -12,5 +12,5 @@ def get_json_text():
     """
 
     with open('data\\text.json', 'r', encoding='utf-8') as file_json:
-        json_ = json.load(file_json)
+        json_: dict = json.load(file_json)
         return json_
